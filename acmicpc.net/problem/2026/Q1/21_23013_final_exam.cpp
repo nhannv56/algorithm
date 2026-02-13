650304fdb9a2ac5007b06f5b914d1ae40dc22cca
Nhan <ngo.nhan.09@gmail.com>
2026-02-13 23:01:44 +0700

    Refactor solve(): const students & interval fixes
    Phương án cover coner case khi it < s < end

--- acmicpc.net/problem/2026/Q1/21_23013_final_exam.cpp
+++ acmicpc.net/problem/2026/Q1/21_23013_final_exam.cpp
@@ -12,85 +12,79 @@ using namespace std;
 
 long long N,M,T;
 
-vector<long long> solve(map<long long, long long>& mp,vector<long long>& students){
+vector<long long> solve(map<long long, long long>& mp,const vector<long long>& students){
     vector<long long> res(students.size(), 0);
-    
-    // for(auto [a, b]: mp){
-    //     cout<<a<<" "<<b<<endl;
-
-    // }
     for(int i = 0; i < students.size();++i){
-        auto it = mp.lower_bound(students[i]);
-        // cout<<"find:"<<students[i]<<endl;
-        if(it != mp.end()){//have item more than student
-            // cout<<"right:"<<it->first<<" "<<it->second<<endl;
-            if(it->first == students[i]){
-                res[i]=it->first;
-                auto a = it->first, b = it->second;
-                mp.erase(it);
-                if(a != b){                    
-                    mp[a+1]= b;
+        const auto s = students[i];
+        auto r = mp.lower_bound(s);
+        auto l = r;
+        if(mp.size() > 0 && r == mp.end()){  //a < s <= b -> end, a <=b < s -> end
+            auto last = std::prev(mp.end());
+            auto a = last->first, b = last->second;
+            if(s <= b){
+                res[i]=s;
+                mp.erase(last);
+                if(a < s){
+                    mp[a]=s-1;
+                }
+                if(s < b){
+                    mp[s+1]=b;
                 }
             }else{
-                if(mp.size() >= 2 && it != mp.begin()){ // có phần tử phía trước
-                    auto l = it;
-                    --l;
-                    // cout<<"left:"<<l->first<<" "<<l->second<<endl;
-                    if(l->second == students[i]){
-                        res[i] = students[i];
-                        auto a = l->first, b = l->second;
-                        mp.erase(l);
-                        if(a != b){
-                            mp[a] =  b - 1;
-                        }
-                    }else if(l->second > students[i]){
-                        res[i] = students[i];
-                        auto a = l->first, b = l->second;
-                        mp.erase(l);
-                        mp[a] = students[i]-1;
-                        mp[students[i]+1] = b;
-                    }else{
-                        //a1 b1 s a2 b2
-                        // cout<<l->first<<" "<<l->second<< " "<<students[i]<<" "<<it->first<<" "<<it->second<<endl;
-                        if(students[i] - l->second <= it->first - students[i]){
-                            res[i] = l->second;
-                            auto a = l->first, b = l->second;
-                            mp.erase(l);
-                            // cout<<"compare:"<<a<<" "<<b<<endl;
-                            if(a != b){
-                                mp[a] = b-1;
-                                // cout<<"insert:"<<a<<" "<<b-1<<endl;
-                            }
-                        }else{
-                            res[i]=it->first;
-                            auto a = it->first, b = it->second;
-                            mp.erase(it);
-                            if(a != b){
-                                mp[a+1] = b;
-                            }
-                        }
-                    }
-                }else{//at begin
-                    res[i]=it->first;
-                    auto a = it->first, b = it->second;
-                    mp.erase(it);
-                    if(a != b){
-                        mp[a+1] = b;
-                    }
+                res[i] = b;
+                if(a < b){
+                    mp[a]=b-1;
+                }else{
+                    mp.erase(last);
                 }
+            }            
+            continue;
+        }
+        if(l != mp.begin()){
+            --l;
+        }
+        if(l==r){// không có item nào bên trái
+            auto a2 = r->first, b2= r->second;
+            res[i] = a2;
+            //đoan a2 < b2 bỏ a2
+            mp.erase(r);
+            if(a2 < b2){ 
+                mp[a2+1] = b2;
             }
-        }else{// all less than students[s]
-            if(mp.size() > 0){
-                --it;
-                res[i] = it->second;
-                auto a = it->first, b = it->second;
-                mp.erase(it);
-                if(a != b){
-                    mp[a] = b-1;
+        }else{
+            auto a1 = l->first, b1 = l->second, a2 = r->first, b2 = r->second;
+            if(s == a2){
+                res[i] = a2;
+                mp.erase(r);
+                if(a2 < b2 ){
+                    mp[a2+1] = b2;
                 }
+            }else{ // s < a2
+                if(s <= b1){
+                    res[i] = s;
+                    mp[a1] = s-1;
+                    if(s < b1){
+                        mp[s+1] = b1;
+                    }
+                }else{ //b1 < s < a2
+                    if(s-b1 <= a2-s){
+                        res[i]=b1;
+                        if(a1 < b1){
+                            mp[a1]=b1-1;
+                        }else{
+                            mp.erase(l);
+                        }
+                    }else{
+                        res[i] = a2;
+                        if(a2 < b2){
+                            mp[a2+1]=b2;
+                        }
+                        mp.erase(r);
+                    }
+                }
+                
             }
         }
-        // cout<<"find:"<<students[i]<<" "<<res[i]<<endl;
     }
     return res;
 }
