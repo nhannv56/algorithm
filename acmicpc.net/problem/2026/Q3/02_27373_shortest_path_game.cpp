a7c82ada15ad9ecc6b32c3be0a5078af74b76b81
Nhan <ngo.nhan.09@gmail.com>
2026-03-12 13:48:28 +0700

    do not break case

--- acmicpc.net/problem/2026/Q3/02_27373_shortest_path_game.cpp
+++ acmicpc.net/problem/2026/Q3/02_27373_shortest_path_game.cpp
@@ -9,15 +9,15 @@ algo: dijiktra
 #include <iterator> // For std::next
 using namespace std;
 struct Edge{
-    int u,v,w;
+    long long u,v,w;
 };
-int T, N, M, Q;
-int total;
-const int INF = 2e8; // Đủ lớn cho hầu hết bài toán
-void floydWarshall(int n, vector<vector<int>>& dist /*, vector<vector<int>>& next_node*/) {
+long long T, N, M, Q;
+long long total;
+const long long INF = 2e8+1; // Đủ lớn cho hầu hết bài toán
+void floydWarshall(long long n, vector<vector<long long>>& dist /*, vector<vector<long long>>& next_node*/) {
     // // next_node dùng để truy vết đường đi
-    // for (int i = 1; i <= n; i++) {
-    //     for (int j = 1; j <= n; j++) {
+    // for (long long i = 1; i <= n; i++) {
+    //     for (long long j = 1; j <= n; j++) {
     //         if (i == j) dist[i][j] = 0;
     //         if (dist[i][j] < INF && i != j) next_node[i][j] = j;
     //         else next_node[i][j] = -1;
@@ -25,9 +25,9 @@ void floydWarshall(int n, vector<vector<int>>& dist /*, vector<vector<int>>& nex
     // }
 
     // 3 vòng lặp O(V^3) - Lưu ý: k phải là vòng lặp ngoài cùng
-    for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+    for (long long k = 1; k <= n; k++) {
+        for (long long i = 1; i <= n; i++) {
+            for (long long j = 1; j <= n; j++) {
                 if (dist[i][k] < INF && dist[k][j] < INF) {
                     if (dist[i][j] > dist[i][k] + dist[k][j]) {
                         dist[i][j] = dist[i][k] + dist[k][j];
@@ -38,63 +38,44 @@ void floydWarshall(int n, vector<vector<int>>& dist /*, vector<vector<int>>& nex
         }
     }
 }
-int countPairsPassingThroughEdge(int u, int v, int w, int n, const vector<vector<int>>& dist) {
-    int count = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (i == j) continue;
-            
-            // Kiểm tra xem cạnh (u, v) có nằm trên lộ trình i -> j không
-            if (dist[i][u] + w + dist[v][j] == dist[i][j] || 
-                dist[i][v] + w + dist[u][j] == dist[i][j]) {
-                count++;
-            }
-        }
-    }
-    // Nếu là đồ thị vô hướng và bạn coi cặp (i, j) giống (j, i), hãy chia 2 kết quả.
-    return count;
-}
-int score(stack<int>& totals){
+
+long long score(stack<long long>& totals){
     return totals.top();
 }
 //dijiktra tìm khoảng cách trước-> tổng
 //thêm/xóa 1 cạnh -> cạnh đó ảnh hưởng tới bao nhieu path 
-void add(int u, int v, int w,stack<Edge>& edges, stack<vector<vector<int>>>& st,stack<int>& totals){
+void add(long long u, long long v, long long w,stack<Edge>& edges, stack<vector<vector<long long>>>& st,stack<long long>& totals){
     
     auto dist = st.top();
     if(w < dist[u][v]){
-        if(dist[u][v] != INF){
-            // dist[u][v]=w;
-            // int reduce = dist[u][v]-w;
-            // cout<<"dis:"<<u<<" "<<v<<" d:"<<dist[u][v]<<endl;
-            for (int i = 1; i <= N; i++) {
-                for (int j = i+1; j <= N; j++) {
-                    if (i == j) continue;
-                    
-                    // Kiểm tra xem cạnh (u, v) có nằm trên lộ trình i -> j không
-                    if (dist[i][j] !=INF && dist[i][u] + w + dist[v][j] < dist[i][j] ) {
-                        int reduce = dist[i][j] -(dist[i][u] + w + dist[v][j]);
+        // dist[u][v]=w;
+        // long long reduce = dist[u][v]-w;
+        // cout<<"dis:"<<u<<" "<<v<<" d:"<<dist[u][v]<<endl;
+        for (long long i = 1; i <= N; i++) {
+            for (long long j = i+1; j <= N; j++) {
+                if (i == j) continue;
+                
+                // Kiểm tra xem cạnh (u, v) có nằm trên lộ trình i -> j không
+                if (dist[i][u] + w + dist[v][j] < dist[i][j] ) {
+                    if(dist[i][j]!=INF){
+                        long long reduce = dist[i][j] -(dist[i][u] + w + dist[v][j]);
                         total-=reduce;
-                        dist[i][j] = dist[i][u] + w + dist[v][j];
-                        dist[j][i] = dist[i][u] + w + dist[v][j];
-                        // cout<<"reduce:"<<i<<" "<<u<<" "<<v<<" "<<j<<" |"<<dist[i][u]<<" "<<reduce<<" "<<dist[v][j]<<endl;
+                    }else{
+                        total+=(dist[i][u] + w + dist[v][j]);
                     }
-                }
-            }
-        }else{
-            // cout<<"case";
-            // dist[u][v]=w;
-            for (int i = 1; i <= N; i++) {
-                for (int j = i+1; j <= N; j++) {
-                    if (i == j) continue;
-                    
-                    // Kiểm tra xem cạnh (u, v) có nằm trên lộ trình i -> j không
-                    if(dist[i][u] != INF && dist[v][j]!= INF && dist[i][j]==INF){
-                        dist[i][j] = dist[i][u] + w + dist[v][j];
-                        dist[j][i] = dist[i][u] + w + dist[v][j];
-                        total += dist[i][j];
-                        // cout<<"add:"<<i<<" "<<u<<" "<<v<<" "<<j<<" |"<<dist[i][u]<<" "<<w<<" "<<dist[v][j]<<endl;
+                    dist[i][j] = dist[i][u] + w + dist[v][j];
+                    dist[j][i] = dist[i][u] + w + dist[v][j];
+                    // cout<<"reduce:"<<i<<" "<<u<<" "<<v<<" "<<j<<" |"<<dist[i][u]<<" "<<reduce<<" "<<dist[v][j]<<endl;
+                }else if (dist[i][v] + w + dist[u][j] < dist[i][j] ) {
+                    if(dist[i][j]!=INF){
+                        long long reduce = dist[i][j] -(dist[i][v] + w + dist[u][j]);
+                        total-=reduce;
+                    }else{
+                        total+=(dist[i][v] + w + dist[u][j]);
                     }
+                    dist[i][j] = dist[i][v] + w + dist[u][j];
+                    dist[j][i] = dist[i][v] + w + dist[u][j];
+                    // cout<<"reduce:"<<i<<" "<<u<<" "<<v<<" "<<j<<" |"<<dist[i][u]<<" "<<reduce<<" "<<dist[v][j]<<endl;
                 }
             }
         }
@@ -105,7 +86,7 @@ void add(int u, int v, int w,stack<Edge>& edges, stack<vector<vector<int>>>& st,
     // Nếu là đồ thị vô hướng và bạn coi cặp (i, j) giống (j, i), hãy chia 2 kết quả.
 }
 
-void erase(stack<Edge>& edges, stack<vector<vector<int>>>& st, stack<int>& totals){
+void erase(stack<Edge>& edges, stack<vector<vector<long long>>>& st, stack<long long>& totals){
     if(edges.size()==0){
         return;
     }
@@ -125,15 +106,16 @@ int main()
     freopen("output.txt", "w", stdout); // Nếu bạn muốn xuất ra file luôn
     #endif
     cin>>T;
-    int u,v,w,r;
+    long long u,v,w,r;
     while(T > 0){
         cin>>N>>M>>Q;
-        vector<vector<int>> dis(N+1, vector<int>(N+1, INF));
-        stack<vector<vector<int>>> sdis;
-        for(int i = 1; i <= N; ++i){
+        vector<vector<long long>> dis(N+1, vector<long long>(N+1, INF));
+        // long long dis[100][100];
+        stack<vector<vector<long long>>> sdis;
+        for(long long i = 1; i <= N; ++i){
             dis[i][i]=0;
         }
-        for(int i = 0 ; i<M;++i){
+        for(long long i = 0 ; i<M;++i){
             cin>>u>>v>>w;
             dis[u][v] = min(dis[u][v],w);
             dis[v][u] = min(dis[u][v],w);
@@ -141,18 +123,18 @@ int main()
         total = 0;
         floydWarshall(N,dis);
         sdis.push(dis);
-        for(int i = 1; i < dis.size(); ++i){
-            for(int j = i+1; j < dis.size();++j){
+        for(long long i = 1; i < dis.size(); ++i){
+            for(long long j = i+1; j < dis.size();++j){
                 if(dis[i][j]!=INF){
                     total+= dis[i][j];
                 }
             }
         }
         stack<Edge> add_edges;
-        stack<int> sum;
+        stack<long long> sum;
         sum.push(total);
         auto orgDis = dis;
-        for(int i = 0; i < Q;++i){
+        for(long long i = 0; i < Q;++i){
             cin>>r;
             if(r==1){
                 cout<<score(sum)<<' ';
