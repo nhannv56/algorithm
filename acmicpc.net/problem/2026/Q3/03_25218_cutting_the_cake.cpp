3559ca346d3a3698c92cc8d0ceabb9d8029533b2
Nhan <ngo.nhan.09@gmail.com>
2026-03-19 13:34:51 +0700

    draft

---
+++ acmicpc.net/problem/2026/Q3/03_25218_cutting_the_cake.cpp
@@ -0,0 +1,95 @@
+/******************************************************************************
+https://www.acmicpc.net/problem/25218
+algo: dp
+algo: prefix sum
+*******************************************************************************/
+#include <iostream>
+#include<bits/stdc++.h>
+#include <iterator> // For std::next
+using namespace std;
+long long T,R,C,N;
+vector<vector<int>> prefixS,prefixB, prefixR;
+string fi;
+long long cal( int i, int r,int c,const vector<string> & cake, vector<vector<vector<long long>>>& dp){
+    if(i<N && r<R && c< C){
+        if(dp[i][r][c] == -1){
+            dp[i][r][c]=0;
+            for(int rr = r+1; rr < R;++rr){
+                //check condition
+                if(fi[i]=='S'){
+                }
+                dp[i][r][c]+= cal(i+1,rr,c,cake, dp);
+            }
+            for(int cc = c+1; cc < C;++cc){
+                //check condition
+                dp[i][r][c]+= cal(i+1,r,cc,cake, dp);
+            }
+        }
+        return dp[i][r][c];
+    }
+    return 0;
+}
+long long solve(vector<string> cake){
+    vector<vector<vector<long long>>> dp(N, vector<vector<long long>>(R, vector<long long>(C,-1)));
+    prefixS = vector<vector<int>>(R, vector<int>(C, 0));
+    prefixB = vector<vector<int>>(R, vector<int>(C, 0));
+    prefixR = vector<vector<int>>(R, vector<int>(C, 0));
+    for(int i = 0; i <R; ++i){
+        for(int j = 0; j < C;++j){
+            switch (cake[i][j])
+            {
+            case 'S':
+                /* code */
+                prefixS[i][j]=1;
+                break;
+            case 'B':
+                prefixB[i][j]=1;
+                break;
+            case 'R':
+                prefixR[i][j]=1;
+                break;
+            default:
+                break;
+            }
+            if(i > 0){
+                prefixS[i][j]+=prefixS[i-1][j];
+                prefixB[i][j]+=prefixB[i-1][j];
+                prefixR[i][j]+=prefixR[i-1][j];
+            }
+            if(j> 0){
+                prefixS[i][j]+=prefixS[i][j-1];
+                prefixB[i][j]+=prefixB[i][j-1];
+                prefixR[i][j]+=prefixR[i][j-1];
+            }
+            if(i > 0 && j > 0){    
+                prefixS[i][j]-=prefixS[i-1][j-1];
+                prefixB[i][j]-=prefixB[i-1][j-1];
+                prefixR[i][j]-=prefixR[i-1][j-1];
+            }
+        }
+    }
+    return cal( 0, 0, 0, cake, dp);
+}
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout); // Nếu bạn muốn xuất ra file luôn
+    #endif
+    cin>>T;
+    while(T>0){
+        cin>>R>>C>>N;
+        vector<string> cake(R);
+        for(int i = 0 ; i < R;++i){
+            cin>>cake[i];
+        }
+        
+        cin>>fi;
+        cout<<solve(cake)<<'\n';
+        --T;
+    }
+	return 0;
+}
