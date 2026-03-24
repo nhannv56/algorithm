f81e5cac8ced85a1d71a477f21cebcdedd3b732a
Nhan <ngo.nhan.09@gmail.com>
2026-03-24 10:06:55 +0700

    PASS

--- acmicpc.net/problem/2026/Q4/05_8972_roboti.cpp
+++ acmicpc.net/problem/2026/Q4/05_8972_roboti.cpp
@@ -9,9 +9,7 @@ https://www.acmicpc.net/problem/8972
 
 using namespace std;
 int R,C;
-struct POS{
-    int x, y, m;
-};
+typedef pair<int,int> POS;
 pair<int,int> playerMove(int x, int y, char ways){
     if(ways == '1'||ways == '4'||ways=='7'){
         --y;
@@ -26,8 +24,6 @@ pair<int,int> playerMove(int x, int y, char ways){
     }
     return {x,y};
 }
-const int PLAYER_POS = 10000;
-const int DESTROY_POS = 10000-1;
 void solve(vector<string> & grids, string moves){
     
     vector<vector<int>> mask(R,vector<int>(C,0));//position of robot and player
@@ -37,69 +33,60 @@ void solve(vector<string> & grids, string moves){
     for(int i = 0; i < R;++i){
         for(int j = 0; j < C;++j){
             if(grids[i][j] == 'I'){
-                p = {i,j,1};
-                mask[i][j]=PLAYER_POS;
+                p = {i,j};
             }else if(grids[i][j]=='R'){
-                robots.push_back({i,j,1});
-                mask[i][j] = 1;
+                robots.push_back({i,j});
             }
         }
     }
     for(int i = 0; i < moves.length(); ++i){     
         //player move
-       
-        auto newPos = playerMove(p.x,p.y, moves[i]);
-        mask[p.x][p.y]=0;
-        p = {newPos.first, newPos.second,1};
-        if(mask[p.x][p.y] == 0){
-            mask[p.x][p.y] = PLAYER_POS;
+        //condition 1 player move
+        auto newPos = playerMove(p.first,p.second, moves[i]);
+        grids[p.first][p.second]='.';
+        p = {newPos.first, newPos.second};
+        if(grids[p.first][p.second] == '.'){
+            grids[p.first][p.second]='I';
         }else{
+            //condition 2 move to robot cell ->end
             cout<<"kraj "<<i+1;
             return;
         }   
-        if(robots.size() > 0){
-            const auto round = robots.front().m;
-            while(!robots.empty() && robots.front().m == round){
-                const auto cur = robots.front(); robots.pop_front();
-                //bug => cung 1 round 2 roboot di den 1 o co roboot -> o co roboot chua di bi bo qua
-                // if(mask[cur.x][cur.y] == DESTROY_POS){
-                //     mask[cur.x][cur.y]=0;
-                //     continue; //Ô nhiều robot bị phá hủy
-                // }
-                //=> xử lý xóa 1 roboot enqueue -> nhưng nó nên đã bị phá hủy
-                POS ne = {cur.x, cur.y, cur.m+1};
-                if(p.x != cur.x) {
-                    cur.x > p.x ? ne.x--:ne.x++;
-                }
-                if(p.y != cur.y) {
-                    cur.y > p.y ? ne.y--:ne.y++;
-                }
-                if(mask[ne.x][ne.y]==PLAYER_POS){
-                    cout<<"kraj "<<i+1;
-                    return;
-                }
-                mask[cur.x][cur.y]=0;
-                if(mask[ne.x][ne.y] == ne.m || mask[ne.x][ne.y] == DESTROY_POS){
-                    // cout<<"destroy:"<<ne.x<<" "<<ne.y<<endl;
-                    mask[ne.x][ne.y] = DESTROY_POS;//ô roboot bị hủy                    
-                }else{
-                    mask[ne.x][ne.y] = ne.m;
-                    robots.push_back(ne);
-                }
+        map<POS, int> robotCount;//check collision
+        for(auto & robot: robots){
+            //condition 3
+            int dx = p.first - robot.first;
+            int dy = p.second - robot.second;
+            if(dx != 0) dx /= abs(dx);
+            if(dy != 0) dy /= abs(dy);
+            auto newRobotPos = make_pair(robot.first + dx, robot.second + dy);
+            //condition 4
+            if(newRobotPos == make_pair(p.first, p.second)){
+                cout<<"kraj "<<i+1;
+                return;
+            }
+			//note lại vị trí robot sẽ đi tới
+            grids[robot.first][robot.second]='.';
+            if(robotCount.find(newRobotPos) != robotCount.end()){
+                robotCount[newRobotPos]++;
+            }else{
+                robotCount[newRobotPos]=1;
             }
         }
-        
+        robots.clear();
+		//cập nhật vị trí mới cho robot
+        for(auto & entry: robotCount){
+            if(entry.second == 1){
+                robots.push_back(entry.first);
+                grids[entry.first.first][entry.first.second]='R';
+            }else{
+                grids[entry.first.first][entry.first.second]='.';
+            }
+        }
+
     }
     for(int i = 0; i < R; ++i){
-        for(int j = 0; j < C;++j){
-            char val = '.';
-            if(mask[i][j] == PLAYER_POS){
-                val = 'I';
-            }else if(mask[i][j] > 0 && mask[i][j] != DESTROY_POS){
-                val = 'R';
-            }
-            cout<<val;
-        }cout<<'\n';
+        cout<<grids[i]<<endl;
     }
 }
 int main()
