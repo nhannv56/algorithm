e459850b1611f445bf2ecaeff31b08d586ea4b56
Nhan <ngo.nhan.09@gmail.com>
2026-03-25 17:25:17 +0700

    accepted solution

--- acmicpc.net/problem/2026/Q4/03_28311_value_of_walls.cpp
+++ acmicpc.net/problem/2026/Q4/03_28311_value_of_walls.cpp
@@ -2,6 +2,10 @@
  * https://www.acmicpc.net/problem/28311
  * algo: bfs
  * algo: graph
+ * algo: dp
+ * dùng bfs nhiều lần tính từ pawn đến mọi vị trí,
+ * từ destiation đến mọi vị tri
+ * khi w bị xóa phần reduce  -> pawn -> empty cell gần w bị xóa -> w -> destation vs pawn -> destiation
  */
 #include <iostream>
 #include <vector>
@@ -14,81 +18,88 @@ using namespace std;
 int R,C,N,r,c;
 int origin = 0, net=0;
 int mv[4][2]={{0,1},{1,0},{-1,0},{0,-1}};
-int bfs(pair<int,int> s,pair<int,int> e, const vector<string>& grid){
-    priority_queue<std::pair<int, pair<int,int>>, 
-                    std::vector<std::pair<int, pair<int,int>>>, 
-                    std::greater<std::pair<int, pair<int,int>>>> pq;
-
-    pq.push({0,s});
+int bfs(const pair<int,int>& s,const vector<string>& grid, vector<vector<int>>& dist,const vector<vector<int>> & pawnGrid){
+    deque<std::pair<int, pair<int,int>>> pq;
+    int res = 0;
+    pq.push_back({0,s});
     vector<vector<bool>> visited(R,vector<bool>(C, false));
     visited[s.first][s.second] = true;
     while(!pq.empty()){
-        auto [step, pos] = pq.top();pq.pop();
+        auto [step, pos] = pq.front();pq.pop_front();
         // cout<<"cur:"<<pos.first<<" "<<pos.second<<'\n';
-        if(pos.first == e.first && pos.second == e.second){
-            return step;
-            break;
-        }
-        for(int i = 0; i < 4; ++i){
-            int nx = pos.first+mv[i][0];
-            int ny = pos.second+mv[i][1];
-            if(nx>=0 && nx <R && ny >=0 && ny<C && !visited[nx][ny] && grid[nx][ny]!='W'){
-                pq.push({step+1,{nx,ny}});
-                visited[nx][ny] = true;
-            }
-        }
-    }
-    return 0;
-}
-int cal(pair<int,int> s, pair<int,int> e, const vector<string>& grid,const vector<int>& dist,vector<vector<int>> &pawnGrid){
-    int wtd = 0;
-    int reduce = 0;
-    priority_queue<std::pair<int, pair<int,int>>, 
-                    std::vector<std::pair<int, pair<int,int>>>, 
-                    std::greater<std::pair<int, pair<int,int>>>> pq;
-
-    pq.push({0,s});
-    vector<vector<bool>> visited(R,vector<bool>(C, false));
-    visited[s.first][s.second] = true;
-    wtd = bfs(s,e,grid);
-    while(!pq.empty()){
-        auto [step, pos] = pq.top();pq.pop();
-        // cout<<"cur:"<<pos.first<<" "<<pos.second<<'\n';
-        
         if(pawnGrid[pos.first][pos.second]!=-1){
-            int item = pawnGrid[pos.first][pos.second];
-            if(step+wtd < dist[item]){
-                reduce+=dist[item] - (step+wtd);
-            }
+            res+= step;
         }
+        dist[pos.first][pos.second] = step;
         for(int i = 0; i < 4; ++i){
             int nx = pos.first+mv[i][0];
             int ny = pos.second+mv[i][1];
             if(nx>=0 && nx <R && ny >=0 && ny<C && !visited[nx][ny] && grid[nx][ny]!='W'){
-                pq.push({step+1,{nx,ny}});
+                pq.push_back({step+1,{nx,ny}});
                 visited[nx][ny] = true;
             }
         }
     }
-    return reduce;
+    return res;
+}
+// Tính toán từ pawn -> w -> destiation reduce
+int cal(const pair<int,int>& s, const vector<string>& grid,const vector<vector<int>>& dist,vector<vector<int>> &pawnGrid,
+    const vector<vector<vector<int>>>& pte, const vector<pair<int,int>>& pawn){
+    int wtd = INT_MAX;
+    int reduce = 0;
+    //min reduce từ wall bị xóa đến dest
+    for(int i = 0; i < 4; ++i){
+        int nx = s.first+mv[i][0];
+        int ny = s.second+mv[i][1];
+        if(nx>=0 && nx <R && ny >=0 && ny<C && grid[nx][ny]!='W'&& dist[nx][ny]!= INT_MAX){
+            // cout<<"wtd:"<<wtd<<endl;
+            wtd = min(wtd,dist[nx][ny]+1);
+            
+        }
+    }
+    //tính lượng reduce pawn -> empty cell gần w -> w -> dest
+    vector<int> ptdreduce(N, 0);//pawn to destiation reduce
+    if(wtd != INT_MAX){
+        for(int i = 0; i < 4; ++i){
+            int nx = s.first+mv[i][0];
+            int ny = s.second+mv[i][1];
+            if(nx>=0 && nx <R && ny >=0 && ny<C && grid[nx][ny]!='W'){
+                for(int j = 0; j < N;++j){
+                    int newDis = INT_MAX;
+                    if(pte[j][nx][ny]!= INT_MAX){
+                        newDis = wtd + pte[j][nx][ny]+1;
+                    }
+                    if(newDis < dist[pawn[j].first][pawn[j].second]){
+                        // Lưu mức reduce tối đa có thể cho 4 lựa chọn
+                        ptdreduce[j] = max(ptdreduce[j], dist[pawn[j].first][pawn[j].second]-newDis);
+                    }
+                }
+            }
+        }
+    }
+    for(auto it : ptdreduce){
+        reduce+=it;
+    }
+    return reduce > 0 ? reduce: 0;
 }
 pair<int,int>solve(const vector<string>& grid,const vector<pair<int,int>>& pawn, int r, int c){
     origin = 0;
     net = 0;
-    vector<int> dist(N, INT_MAX);
-    for(int i = 0; i < N;++i){
-        int dis = bfs(pawn[i],{r,c},grid);
-        dist[i] = dis;            
-        origin+=dis;
-    }
-    vector<vector<int>>pawnGrid(R,vector<int>(C, -1));
+    vector<vector<int>> dist(grid.size(),vector<int>(grid[0].length(),INT_MAX));
+    vector<vector<int>> pawnGrid(R,vector<int>(C, -1));
     for(int i = 0; i < pawn.size();++i){
         pawnGrid[pawn[i].first][pawn[i].second]= i;
+    }
+    origin = bfs({r,c}, grid, dist, pawnGrid);
+    //pawn to every w
+    vector<vector<vector<int>>> pte(N,vector<vector<int>>(grid.size(),vector<int>(grid[0].length(),INT_MAX)));
+    for(int i = 0; i < pawn.size();++i){
+        bfs(pawn[i],grid,pte[i],pawnGrid);
     }
     for(int i = 0; i < R; ++i){
         for(int j = 0; j < C; ++j){
             if(grid[i][j]=='W'){
-                net+=cal({i,j},{r,c},grid,dist,pawnGrid);
+                net+=cal({i,j},grid,dist,pawnGrid,pte, pawn);
             }
         }
     }
