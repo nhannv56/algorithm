ddac1ef9358d936311e2f2099c0d17328c0e50a1
Nhan <ngo.nhan.09@gmail.com>
2026-03-26 21:50:06 +0700

    Add comments on binary representation of x
    Add comments explaining the binary representation of x and its implications.

--- acmicpc.net/problem/2026/Q4/6_30300_and_mex.cpp
+++ acmicpc.net/problem/2026/Q4/6_30300_and_mex.cpp
@@ -2,4 +2,11 @@
 x luôn có dạng bin là 1,11,111,1111 để đảm bảo luôn sinh ra các số 000,001,010,011,100,...
 Vi du neu x la 10 -> ko the sinh ra số 11
 -> kiếm tra vs x
+
+00001101
+00001011
+00101000
+00001010
+00100001
+00010011
 **/
