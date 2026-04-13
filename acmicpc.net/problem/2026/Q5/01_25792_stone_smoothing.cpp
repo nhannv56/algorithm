939d73b2c7bf486bd6680788709145ca5019ebf4
Nhan <ngo.nhan.09@gmail.com>
2026-04-13 10:52:36 +0700

    Refactor stone smoothing algorithm implementation
    Refactor stone smoothing algorithm to improve clarity and efficiency. Added mathematical checks and updated angle calculations.

--- acmicpc.net/problem/2026/Q5/01_25792_stone_smoothing.cpp
+++ acmicpc.net/problem/2026/Q5/01_25792_stone_smoothing.cpp
@@ -1,96 +1,99 @@
 /***
 
 https://www.acmicpc.net/problem/25792
-dùng map<double,int> tham số góc và số góc tương ứng
-khởi tạo C góc ban đầu
-duyệt từ góc nhỏ tìm góc kế tiếp,
-chia sao cho góc nhỏ > góc liền kề -> xóa + chèn vào map
-công thức 180 - (180-alpha)/(2*k)
--alpha là góc đang xét
--k là số lần cắt sẽ tạo ra k góc mới
+algo: binary search
+algo: math
+góc ngoài mới = góc ngoài cũ chia số góc -1 => số lần cắt
+Tìm góc ngoài theo toán học nhỏ nhất có thể thỏa mãn điều kiện
+tìm số lần cắt tối thiểu để đạt được cho các gọc hiện có
 **/
 #include <iostream>
-#include <bits/stdc++.h>
-#include <iterator> // For std::next
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+
 using namespace std;
-int C, S;
-const double PI = 3.14159265358979323846;
-bool satify(double minDegree, int numCut, vector<double> degrees)
-{
-    int needCut = 0;
-    bool res = true;
-    for (int i = 0; i < degrees.size(); ++i)
-    {
-        if (degrees[i] < minDegree)
-        {
-            needCut = ceil(degrees[i]/(PI-minDegree)-1);
-            numCut -= needCut;
-            if (numCut < 0)
-            {
-                res = false;
-                break;
-            }
+
+const double PI = acos(-1.0);
+
+struct Point {
+    double x, y;
+};
+
+// Hàm kiểm tra xem với góc ngoài tối đa là maxExtAngle, số lần cắt có <= S không
+bool check(double maxExtAngle, long long S, const vector<double>& extAngles) {
+    long long totalCuts = 0;
+    for (double alpha : extAngles) {
+        if (alpha > maxExtAngle) {
+            // Tính số nhát cắt cần thiết để chia góc alpha thành các phần <= maxExtAngle
+            // Số phần = ceil(alpha / maxExtAngle), nên số nhát cắt = Số phần - 1
+            long long cuts = ceil(alpha / maxExtAngle - 1e-9) - 1;
+            S -= max(0LL, cuts);
         }
     }
-    cout<<"D:"<<minDegree*180/PI<<" "<<res<<endl;
-    return res;
+    return S >= 0;
 }
-int main()
-{
-    std::ios_base::sync_with_stdio(false);
+
+int main() {
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
 
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout); // Nếu bạn muốn xuất ra file luôn
 #endif
-    cin >> C >> S;
-    double x, y;
-    vector<pair<double, double>> pos(C + 2);
-    for (int i = 0; i < C; ++i)
-    {
-        cin >> pos[i].first >> pos[i].second;
+    int C;
+    long long S;
+    if (!(cin >> C >> S)) return 0;
+
+    vector<Point> pts(C);
+    for (int i = 0; i < C; ++i) {
+        cin >> pts[i].x >> pts[i].y;
     }
-    pos[C] = pos[0];
-    pos[C + 1] = pos[1];
-    // build góc
-    //  solve
-    vector<double> degrees;
-    for (int i = 1; i < pos.size() - 1; ++i)
-    {
-        auto [xa, ya] = pos[i - 1];
-        auto [xb, yb] = pos[i];
-        auto [xc, yc] = pos[i + 1];
-        auto b1 = xa - xb, b2 = ya - yb;
-        auto c1 = xc - xb, c2 = yc - yb;
-        auto arccosVal = (b1 * c1 + b2 * c2) / (sqrt(b1 * b1 + b2 * b2) * sqrt(c1 * c1 + c2 * c2));
-        auto radian = acos(arccosVal);
-        // auto d = radian * 180.0 / PI; // conver radian to o
-        // cout<<"cos:"<<arccosVal<<" "<<d<<'\n';
-        degrees.push_back(radian);
+
+    vector<double> extAngles(C);
+    for (int i = 0; i < C; ++i) {
+        Point prev = pts[(i - 1 + C) % C];
+        Point curr = pts[i];
+        Point next = pts[(i + 1) % C];
+
+        // Vector BA và BC
+        double v1x = prev.x - curr.x;
+        double v1y = prev.y - curr.y;
+        double v2x = next.x - curr.x;
+        double v2y = next.y - curr.y;
+
+        // Tính góc trong (interior angle) bằng dot product
+        double dotProduct = v1x * v2x + v1y * v2y;
+        double mag1 = sqrt(v1x * v1x + v1y * v1y);
+        double mag2 = sqrt(v2x * v2x + v2y * v2y);
+        
+        double cosVal = dotProduct / (mag1 * mag2);
+        // Tránh lỗi độ chính xác floating point làm cosVal vượt quá [-1, 1]
+        cosVal = max(-1.0, min(1.0, cosVal));
+        
+        double interiorAngle = acos(cosVal); // radian
+        extAngles[i] = PI - interiorAngle;   // Góc ngoài = PI - góc trong
     }
-    // sort(degrees.begin(), degrees.end());
-    // for(auto d : degrees){
-    //     cout<<d<<endl;
-    // }
-    double l = 0, r = PI, mid;
-    // double minDegree = *degrees.begin();
-    double res = 0;
-    for (int i = 0; i < 100; ++i)
-    {
-        double mid = (l + r) / 2.0;
-        if (satify(mid, S, degrees))
-        {
-            l = mid;
-            res = l;
+
+    // Tìm kiếm nhị phân cho góc ngoài mục tiêu (từ 0 đến PI)
+    double left = 0.0;
+    double right = PI;
+    
+    // 200 vòng lặp là quá đủ cho độ chính xác của số thực
+    for (int iter = 0; iter < 200; ++iter) {
+        double mid = (left + right) / 2.0;
+        if (check(mid, S, extAngles)) {
+            right = mid; // Có thể đạt được, thử ép góc ngoài nhỏ hơn nữa
+        } else {
+            left = mid;  // Không đủ số lần cắt, phải chấp nhận góc ngoài lớn hơn
         }
-        else
-        {
-            r = mid;
-            res = r;
-        }
     }
-    cout << (2*PI - res)* 180.0 / PI << '\n';
+
+    // Kết quả là góc trong nhỏ nhất tương ứng (chuyển đổi sang hệ Độ)
+    double bestMinInteriorAngle = PI + right;
+    cout << fixed << setprecision(6) << (bestMinInteriorAngle * 180.0 / PI) << "\n";
+
     return 0;
 }
