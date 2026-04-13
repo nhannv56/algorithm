995a988af3c829219e3b3f30ecfc6bb7ccab9964
Nhan <ngo.nhan.09@gmail.com>
2026-04-13 11:32:41 +0700

    Refactor stone smoothing algorithm for clarity

--- acmicpc.net/problem/2026/Q5/01_25792_stone_smoothing_2.cpp
+++ acmicpc.net/problem/2026/Q5/01_25792_stone_smoothing_2.cpp
@@ -2,34 +2,34 @@
 https://www.acmicpc.net/problem/25792
 needCut = ceil((PI - degrees[i]) / (PI - minDegree));
 Sử dụng góc trong
+user write
 **/
 #include <iostream>
 #include <bits/stdc++.h>
 #include <iterator> // For std::next
 using namespace std;
-long long C;
+long long C, S;
 const double PI =  acos(-1.0);
-struct Point {
-    double x, y;
-};
-bool satify(double targetMinInt, long long S, vector<double>& intAngles)
+bool satify(double minDegree, long long numCut, vector<double>& degrees)
 {
-    for (double currentInt : intAngles) {
-        if (currentInt < targetMinInt) {
-            double divisor = PI - targetMinInt;
-            // Nếu targetMinInt rất gần PI, số nhát cắt sẽ tiến tới vô cực
-            if (divisor < 1e-12) return false;
-            
-            // Tính số nhát cắt cần thiết để phân bổ góc ngoài
-            // Góc ngoài mục tiêu = PI - targetMinInt
-            // Góc ngoài hiện tại = PI - currentInt
-            long long cuts = ceil((PI - currentInt) / divisor - 1e-9) - 1;
-            S -= max(0LL, cuts);
-            
-            if (S < 0) return false; // Không đủ số lần cắt
+    long long needCut = 0;
+    bool res = true;
+    for (int i = 0; i < degrees.size(); ++i)
+    {
+        if (degrees[i] < minDegree)
+        {
+            needCut = ceil((PI-degrees[i])/(PI-minDegree)- 1e-9) - 1;
+            needCut = max(0LL, needCut); // Đảm bảo số lần cắt không bị âm
+            numCut -= needCut;
+            if (numCut < 0)
+            {
+                res = false;
+                break;
+            }
         }
     }
-    return true;
+    // cout<<"D:"<<minDegree*180/PI<<" "<<res<<endl;
+    return res;
 }
 int main()
 {
@@ -41,50 +41,53 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout); // Nếu bạn muốn xuất ra file luôn
 #endif
-long long S;
     cin >> C >> S;
     double x, y;
-    vector<Point> pts(C);
-    for (int i = 0; i < C; ++i) {
-        cin >> pts[i].x >> pts[i].y;
+    vector<pair<double, double>> pos(C + 2);
+    for (int i = 0; i < C; ++i)
+    {
+        cin >> pos[i].first >> pos[i].second;
     }
-
-    vector<double> intAngles(C);
-    for (int i = 0; i < C; ++i) {
-        Point prev = pts[(i - 1 + C) % C];
-        Point curr = pts[i];
-        Point next = pts[(i + 1) % C];
-
-        // Vector BA và BC
-        double v1x = prev.x - curr.x;
-        double v1y = prev.y - curr.y;
-        double v2x = next.x - curr.x;
-        double v2y = next.y - curr.y;
-
-        double dotProduct = v1x * v2x + v1y * v2y;
-        double mag1 = sqrt(v1x * v1x + v1y * v1y);
-        double mag2 = sqrt(v2x * v2x + v2y * v2y);
-        
-        double cosVal = dotProduct / (mag1 * mag2);
-        cosVal = max(-1.0, min(1.0, cosVal));
-        
-        // Lưu lại chính góc trong (interior angle)
-        intAngles[i] = acos(cosVal);
+    pos[C] = pos[0];
+    pos[C + 1] = pos[1];
+    // build góc
+    //  solve
+    vector<double> degrees;
+    for (int i = 1; i < pos.size() - 1; ++i)
+    {
+        auto [xa, ya] = pos[i - 1];
+        auto [xb, yb] = pos[i];
+        auto [xc, yc] = pos[i + 1];
+        auto b1 = xa - xb, b2 = ya - yb;
+        auto c1 = xc - xb, c2 = yc - yb;
+        auto arccosVal = (b1 * c1 + b2 * c2) / (sqrt(b1 * b1 + b2 * b2) * sqrt(c1 * c1 + c2 * c2));
+        arccosVal = max(-1.0, min(1.0, arccosVal));
+        auto radian = acos(arccosVal);
+        // auto d = radian * 180.0 / PI; // conver radian to o
+        // cout<<"cos:"<<arccosVal<<" "<<d<<'\n';
+        degrees.push_back(radian);
     }
-    double l = 0.0, r = PI, mid;
+    // sort(degrees.begin(), degrees.end());
+    // for(auto d : degrees){
+    //     cout<<d<<endl;
+    // }
+    double l = *min_element(degrees.begin(), degrees.end()), r = PI, mid;
+    // double minDegree = *degrees.begin();
     double res = 0;
     for (int i = 0; i < 200; ++i)
     {
         double mid = (l + r) / 2.0;
-        if (satify(mid, S, intAngles))
+        if (satify(mid, S, degrees))
         {
             l = mid;
+            res = l;
         }
         else
         {
             r = mid;
+            res = r;
         }
     }
-    cout <<fixed << setprecision(6) << 360-(l * 180.0 / PI) << "\n";
+    cout << fixed << setprecision(6) << 360-res * 180.0 / PI << '\n';
     return 0;
 }
