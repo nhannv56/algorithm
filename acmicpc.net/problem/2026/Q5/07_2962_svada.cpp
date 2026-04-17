59842fe7ca7944bb82ffdcc40c6293cf991ce89c
Nhan <ngo.nhan.09@gmail.com>
2026-04-17 13:57:20 +0700

    Refactor coconut counting and time calculation logic

--- acmicpc.net/problem/2026/Q5/07_2962_svada.cpp
+++ acmicpc.net/problem/2026/Q5/07_2962_svada.cpp
@@ -1,5 +1,8 @@
 /***
 https://www.acmicpc.net/problem/2962
+2 lần binary search
+lần 1: tìm thời gian cần thiết để có được T quả dừa
+lần 2: tìm số dừa của mỗi con khỉ tại thời gian đó
 **/
 #include <iostream>
 #include <bits/stdc++.h>
@@ -9,36 +12,35 @@ struct monkey
 {
     long long a, b;
 };
-pair<long long, long long> countCoconut(vector<monkey> &m, long long time)
+long long countCoconut(vector<monkey> &m, long long time)
 {
-    long long mi = 0, ma = 0;
-    bool valid = false;
+    long long total = 0;
     for (int i = 0; i < m.size(); ++i)
     {
         if (time >= m[i].a)
         {
-            if(time==m[i].a){
-                mi+=1;
-                ma+=1;
-                valid = true;
-                continue;
-            }
-            if ((time - m[i].a) % m[i].b == 0 && !valid)
-            {
-                valid = true;
-                mi+= (time - m[i].a) / m[i].b + 1;
-            }else if((time - m[i].a) % m[i].b == 0){
-                mi+= (time - m[i].a) / m[i].b;
-            }else{
-                mi+= (time - m[i].a) / m[i].b+1;
-            }
-            // mi+= (time - m[i].a) / m[i].b;
-            ma += (time - m[i].a) / m[i].b + 1;
-        }else{
-            return make_pair(-1LL, -1LL);
+            total += (time - m[i].a) / m[i].b + 1;
         }
     }
-    return (valid ? make_pair(mi, ma) : make_pair(-1LL, -1LL));
+    return total;
+}
+//tìm thời gian cần thiết để có được coconutCount quả dừa
+long long timeNeed(vector<monkey> &m, long long coconutCount)
+{
+    long long l = 0, r = 1e12;//time
+    while (l < r)
+    {
+        long long mid = (l + r) / 2;
+        if (countCoconut(m, mid) >= coconutCount)
+        {
+            r = mid;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return l;
 }
 int main()
 {
@@ -64,39 +66,26 @@ int main()
     {
         cin >> m2[i].a >> m2[i].b;
     }
-    long long l = 0, r = T;
-    long long aCount = 0, bCount = 0;
+    //binary search theo số dừa hái được để tìm thời gian cần thiết để có được T quả dừa
+    long long l = 0, r = T*M;
     long long res = 0;
     while (l <= r)
     {
-        int mid = (l + r) / 2;
-        long long sum1 = 0, sum2 = 0;
-        auto aCount = countCoconut(m1, mid);
-        auto bCount = countCoconut(m2, T - mid);
-        // cout<<mid<<" |"<<aCount.first<<" "<<aCount.second<<" "<<bCount.first<<" "<<bCount.second<<"\n";
-        if (aCount.first == -1)
+        long long mid = (l + r) / 2;
+        long long t1 = timeNeed(m1, mid);
+        long long t2 = timeNeed(m2, mid);
+        if (t1 + t2 == T)
+        {
+            res = t1;
+            break;
+        }
+        else if (t1 + t2 < T)
         {
             l = mid + 1;
-            continue;
-        }
-        if (bCount.first == -1)
-        {
-            r = mid - 1;
-            continue;
-        }
-        if (aCount.second < bCount.first)
-        {
-            l = mid + 1;
-        }
-        else if(aCount.first > bCount.second)
-        {
-            r = mid - 1;
         }
         else
         {
-            res = mid;
-            l = mid + 1;
-            break;
+            r = mid - 1;
         }
     }
     cout << res;
