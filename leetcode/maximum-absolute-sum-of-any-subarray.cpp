28e8f445d73ef4adb8942157e20ab548b9a9788c
Nhan <ngo.nhan.09@gmail.com>
2026-05-18 10:17:05 +0700

    Implement maxAbsoluteSum function for absolute sum
    Add initial implementation of maxAbsoluteSum function.

---
+++ leetcode/maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,20 @@
+/***
+  https://leetcode.com/problems/maximum-subarray/description/
+  algo: kanda's algorithm
+  algo: dp (minVal is dp at[i-1] max sub array ket thuc taij i-1
+  **/
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        
+        int minVal = 0,  maxVal = 0;
+        int res = 0;
+        for(auto val: nums){
+            minVal = min({minVal+val,val});
+            maxVal = max({maxVal+val,val});
+            // cout<<minVal<<" "<<maxVal<<endl;
+            res = max({res, abs(minVal), abs(maxVal)});
+        }
+        return res;
+    }
+};
