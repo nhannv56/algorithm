6606ef02fe73189755687f4bfa6c1612faff1770
Nhan <ngo.nhan.09@gmail.com>
2026-05-18 11:08:43 +0700

    Add maximum product subarray solution
    Implement Kadanes' algorithm to find the maximum product subarray using dynamic programming.

---
+++ leetcode/maximum-product-subarray.cpp
@@ -0,0 +1,24 @@
+/***
+algo: kandanes' algo
+algo: dp multipe lưu lại max dương và min âm
+  **/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int mV=1,pV =1;
+        int res = nums[0];
+        for(int i = 0; i < nums.size();++i){
+            if(nums[i]==0){
+                mV=1;
+                pV=1;
+                res = max(res, 0);
+            }else{
+                auto tMV = mV, tPV = pV;
+                mV = min({tPV*nums[i],tMV*nums[i],nums[i]});
+                pV = max({tPV*nums[i],tMV*nums[i],nums[i]});
+                res = max({mV,pV,res});
+            }
+        }
+        return res;
+    }
+};
