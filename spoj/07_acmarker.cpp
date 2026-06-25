f30d880fb38434df1683f1be7fb1177395d04a34
Nhan <ngo.nhan.09@gmail.com>
2026-06-25 11:35:02 +0700

    Update 07_acmarker.cpp

--- spoj/07_acmarker.cpp
+++ spoj/07_acmarker.cpp
@@ -1,15 +1,16 @@
 /******************************************************************************
 
 https://spoj.com/problems/ACMAKER/
-algo: dp
-viết lại dp vì mỗi chữ cần đóng góp it nhất 1 ký tự
+algo: dp string
+algo: dp 4 chiều
+mỗi từ đóng góp ít nhất 1 ký tự
 *******************************************************************************/
 #include <iostream>
 #include <bits/stdc++.h>
 #include <cctype>
 using namespace std;
 long long N, Q;
-int cal(const string &text, const string &target,const int i,const int j, vector<vector<int>>& dp){
+int cal_word(const string &text, const string &target,const int i,const int j, vector<vector<int>>& dp){
     if(i < text.size() && j < target.size()){
         if(dp[i][j] == -1){
             if(i == text.length()-1 && j == target.length()-1){
@@ -24,9 +25,9 @@ int cal(const string &text, const string &target,const int i,const int j, vector
                     dp[i][j]++;
                 }
                 if(text[i]==target[j]){
-                    dp[i][j]+= cal(text,target,i+1,j+1,dp);
+                    dp[i][j]+= cal_word(text,target,i+1,j+1,dp);
                 }
-                dp[i][j]+=cal(text,target,i+1,j,dp);
+                dp[i][j]+=cal_word(text,target,i+1,j,dp);
             }
         }
         return dp[i][j];
@@ -34,20 +35,41 @@ int cal(const string &text, const string &target,const int i,const int j, vector
         return 0;
     }
 }
-void solve(const string &text, string &target)
+//tìm số cách ghép target từ text
+int dp_word(const string &text, string &target)
 {
     int res = 0;
     vector<vector<int>> dp(text.size(), vector<int>(target.size(), -1));
-    std::transform(target.begin(), target.end(), target.begin(), [](unsigned char c) {
-        return std::tolower(c);
-    });
-    res = cal(text, target, 0, 0, dp);
-
-    if(res == 0){
-        cout<<target<<" is not a valid abbreviation\n";
-    }else{
-        cout<<target<<" can be formed in "<<res<<" ways\n";
+    
+    res = cal_word(text, target, 0, 0, dp);
+    // cout<<"dp_word: find:"<<target<<" in:"<<text<<" res: "<<res<<"\n";
+    return res;
+}
+int solve(const vector<string> &text, string target, int wi, vector<vector<int>>& dp)
+{
+    // cout<<"wi: "<<wi<<" target: "<<target<<"\n";
+    // if(target.length() > text.size() - wi){
+    //     cout<<target<<" case text too short\n";
+    //     return 0;
+    // }
+    if(wi == text.size() -1 && target.length() > 0){
+        return dp_word(text[wi], target);//nếu chỉ còn 1 từ cuối cùng thì tìm cách ghép target với từ cuối cùng
     }
+    if(dp[wi][target.length()] != -1){//nếu đã tính toán trước đó thì trả về kết quả
+        return dp[wi][target.length()];
+    }
+    int res = 0;
+    int maxlen = target.length() - (text.size() - wi -1);
+    // cout<<"target: "<<target<<" maxlen: "<<maxlen<<"\n";
+    for(int i = 0; i<maxlen; ++i){ //duyệt tất cả các ký tự của target để tìm cách ghép với text[wi]
+        string t = target.substr(0, i+1);
+        int left = dp_word(text[wi], t);
+        if(left > 0){
+            res += left * solve(text, target.substr(i+1), wi+1, dp);//tìm cách ghép phần còn lại của target với các từ còn lại
+        }
+    }
+    dp[wi][target.length()] = res;
+    return res;    
 }
 int main()
 {
@@ -78,14 +100,25 @@ int main()
                 stringstream ss(line);
                 string Target,word;
                 ss>> Target;
-                string text="";
+                string ltarget = Target;
+                for(int i =0; i<Target.length(); ++i){
+                    ltarget[i] = std::tolower(Target[i]);
+                }
+                vector<string> text;
                 while(ss>>word){
                     if(ignoredWords.find(word) == ignoredWords.end()){
-                        text+=word;
+                        text.push_back(word);
                     }
                 }                
-                cout<<text<<" "<<Target<<"\n";
-                solve(text, Target);
+                // cout<<Target<<"\n";
+                vector<vector<int>> dp(text.size()+1, vector<int>(ltarget.size()+1, -1));
+                int res = solve(text, ltarget, 0, dp);
+                if(res > 0){
+                    cout<<Target<<" can be formed in "<<res<<" ways\n";
+                }else{
+                    cout<<Target<<" is not a valid abbreviation\n";
+                }
+                // cout<<"--------------------------------\n";
             }
         }
     }
