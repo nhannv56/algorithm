0a58fde157828506dfc173b0ce9865d776d8b200
Nhan <ngo.nhan.09@gmail.com>
2026-07-06 08:51:19 +0700

    Add solution for remove covered intervals problem
    Implement solution for removing covered intervals using sorting.

---
+++ leetcode/017-remove-covered-intervals.cpp
@@ -0,0 +1,43 @@
+/**
+algo: sorting
+https://leetcode.com/problems/remove-covered-intervals/?envType=daily-question&envId=2026-07-06
+tìm xem có bao nhiêu đoạn nằm trong các đoạn khác
+**/
+class Solution {
+    class Node{
+        public:
+        int l, r;
+        bool operator < (Node & b)const{
+            if(l != b.l){
+                return l < b.l;
+            }else{
+                return r > b.r;
+            }
+        };
+    };
+    
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        vector<Node> sortInterval;
+        for (auto iv : intervals) {
+            sortInterval.push_back({iv[0],iv[1]});
+        }
+        sort(sortInterval.begin(), sortInterval.end());
+        // for(auto [l,r]: sortInterval){
+        //     cout<<l <<" "<<r<<endl;
+        // }
+        auto [fl, fr] = sortInterval[0];
+        int maxLen = fr-fl+1;
+        int res = intervals.size();
+        for(int i = 1; i < sortInterval.size();++i){
+            auto [l, r] = sortInterval[i];
+            int curLen = r-l+1;
+            maxLen = maxLen - (l - sortInterval[i-1].l);
+            if(maxLen >=curLen){
+                res--;
+            }
+            maxLen =max(maxLen, curLen);
+        }
+        return res;
+    }
+};
