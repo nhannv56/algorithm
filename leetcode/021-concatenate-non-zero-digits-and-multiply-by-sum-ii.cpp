90fad42cffd4295cb67ecf7c022ffad08a66d44a
Nhan <ngo.nhan.09@gmail.com>
2026-07-08 15:47:20 +0700

    Update 021-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp

--- leetcode/021-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ leetcode/021-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -2,6 +2,20 @@
 algo: string
 algo: prefix
 algo: precompute, module cua so lon
+You are given a string s of length m consisting of digits. You are also given a 2D integer array queries, where queries[i] = [li, ri].
+
+For each queries[i], extract the substring s[li..ri]. Then, perform the following:
+
+Form a new integer x by concatenating all the non-zero digits from the substring in their original order. If there are no non-zero digits, x = 0.
+Let sum be the sum of digits in x. The answer is x * sum.
+Return an array of integers answer where answer[i] is the answer to the ith query.
+
+Since the answers may be very large, return them modulo 109 + 7.
+1 <= m == s.length <= 105
+s consists of digits only.
+1 <= queries.length <= 105
+queries[i] = [li, ri]
+0 <= li <= ri < m
 **/
 const int MOD = 1e9 + 7;
 const int MAX_N = 100001;
