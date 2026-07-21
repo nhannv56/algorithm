62632b5e6fd0f331ef5e4260de7044e88dfacb92
Nhan <ngo.nhan.09@gmail.com>
2026-07-21 21:21:18 +0700

    Fix swirl game algorithm and add test cases
    Rewrote the recursive algorithm in the cal() function to correctly handle spiral traversal for all four corner starting points. Fixed base cases, index calculations, and substring extraction logic. Added logic to differentiate between cases where a==b versus a!=b, and handle different directions based on position relative to center. Corrected return value ordering and added test input/output files for validation.

--- codejam/2021-final/23019_swirl_game.cpp
+++ codejam/2021-final/23019_swirl_game.cpp
@@ -8,40 +8,51 @@ https://codejam.lge.com/problem/23019
 using namespace std;
 
 long long N, T;
-pair<string,string> cal(vector<string> & row,vector<string> & cols,vector<string> & rrow,vector<string> & rcols, int a, int b,int s){
-    if(N%2==1 && a==b && a==N/2){
-        return {to_string(row[a][a]),to_string(row[a][a])};
+pair<string,string> cal(vector<string> & row,vector<string> & cols,vector<string> & rrow,vector<string> & rcols, int a, int b,const int s){
+    if(s==1){
+        string s = "";
+        s.push_back(row[a][a]);
+        return {s,s};
     }
     if(s==0){
         return {"",""};
     }
-    int i = N-s;
-    string ri = row[i].substr(i,s)+cols[s-1].substr(i+1,s-1)+rrow[s-1].substr(i+1,s-1)+rcols[i].substr(i+1,s-2);
-    string le = cols[i].substr(i,s)+row[s-1].substr(i+1,s-1)+rcols[s-1].substr(i+1,s-1)+rrow[i].substr(i+1,s-2);
-    cout<<ri<<" "<<le<<endl;
-    // if(a==b){
-    //     if(a < N/2){
-    //         string ri = row[a].substr(b,s)+cols[s-1].substr(a+1,s-1)+rrow[N-a].substr(b+1,s-1)+rcols[a].substr(b+1,s-2);
-    //         string le = cols[b].substr(a,s)+row[s-1].substr(b+1,s-1)+rcols[s-1].substr(b+1,s-1)+rrow[a].substr(b+1,s-2);
-    //         auto [mi,ma] = cal(row,cols,rrow,rcols,a+1,b+1,s-2);
-    //         return {min(ri,le)+mi,max(ri,le)+ma};
-    //     }else{
-    //         string ri = rrow[N-a].substr(b+1,s-1)+rcols[a].substr(b+1,s-2)+row[a].substr(b,s)+cols[s-1].substr(a+1,s-1);
-    //         string le = rcols[s-1].substr(b+1,s-1)+rrow[a].substr(b+1,s-2)+cols[b].substr(a,s)+row[s-1].substr(b+1,s-1);
-    //         auto [mi,ma] = cal(row,cols,rrow,rcols,a-1,b-1,s-2);
-    //         return {min(ri,le)+mi,max(ri,le)+ma};
-    //     }
-    // }else{
-    //     if(a < b){
-    //         string ri = row[a].substr(b,s)+cols[s-1].substr(a+1,s-1)+rrow[N-a].substr(b+1,s-1)+rcols[a].substr(b+1,s-2);
-    //         string le = cols[b].substr(a,s)+row[s-1].substr(b+1,s-1)+rcols[s-1].substr(b+1,s-1)+rrow[a].substr(b+1,s-2);
-    //         auto [mi,ma] = cal(row,cols,rrow,rcols,a+1,b+1,s-2);
-    //         return {min(ri,le)+mi,max(ri,le)+ma};
-    //     }else{
-
-    //     }
-    // }
-    return {"",""};
+    int i = (N-s)/2;
+    string ri = row[i].substr(i,s-1)+cols[N-i-1].substr(i,s-1)+rrow[N-i-1].substr(i,s-1)+rcols[i].substr(i,s-1);
+    ri+=ri;
+    string le = cols[i].substr(i,s-1)+row[N-i-1].substr(i,s-1)+rcols[N-i-1].substr(i,s-1)+rrow[i].substr(i,s-1);
+    le+=le;
+    string s1,s2;
+    int na,nb;
+    if(a==b ){
+        if(a < N/2)
+        {
+            s1 = ri.substr(0,s*4-4);
+            s2= le.substr(0,s*4-4);
+            na=a+1;
+            nb=b+1;
+        }else{
+            s1=ri.substr(2*s-2,s*4-4);
+            s2=le.substr(2*s-2,s*4-4);
+            na=a-1;
+            nb=b-1;
+        }
+    }else{
+        if(a < b){
+            s1 = ri.substr(s-1,s*4-4);
+            s2= le.substr(3*s-3,s*4-4);
+            na = a+1;
+            nb=b-1;
+        }else{
+            s1 = ri.substr(3*s-3,s*4-4);
+            s2= le.substr(s-1,s*4-4);
+            na=a-1;
+            nb=b+1;
+        }
+    }
+    auto [ma,mi] = cal(row,cols,rrow,rcols,na,nb,s-2);
+    auto r1 = max(s1,s2)+ma, r2 = min(s1,s2)+mi;
+    return {r1,r2};
 }
 pair<string, string> solve(vector<string> & row){
     vector<string> cols(row[0].length()), rcols(row[0].length()), rrow(row.size());
@@ -51,6 +62,7 @@ pair<string, string> solve(vector<string> & row){
         }
         rcols[i]= cols[i];
         reverse(rcols[i].begin(), rcols[i].end());
+        // cout<<rcols[i]<<endl;
     }
     for(int i = 0; i < row.size();++i){
         rrow[i]= row[i];
@@ -59,11 +71,16 @@ pair<string, string> solve(vector<string> & row){
     vector<pair<int,int>> points = {{0,0},{0, N-1},{N-1,0},{N-1,N-1}};
     string mi, ma;
     for(auto [x,y] : points){
+        // cout<<"solve:"<<x<<" "<<y<<endl;
         auto [a,b] = cal(row,cols, rrow,rcols, x,y,N);
-        mi = min(mi,a);
-        ma = max(ma,b);
+        if(mi.empty()){
+            ma = a;
+            mi = b;
+        }
+        ma = max(ma,a);
+        mi = min(mi,b);
     }
-    return{mi,ma};
+    return{ma,mi};
 }
 int main()
 {
@@ -75,7 +92,6 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
     cin >> T;
-    int N;
     double X,Y;
     while(T > 0){
         cin>>N;
@@ -84,7 +100,7 @@ int main()
             cin>>gr[i];
         }
         auto [ma,mi] = solve(gr);
-        cout<<ma<<' '<<mi<<endl;
+        cout<<ma<<" "<<mi<<endl;
         --T;
     }
     return 0;
